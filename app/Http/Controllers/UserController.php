069449c8b9cb5b96d850f8c3c3cc7d7d4a1a1f13
natacha-h <natacha.heubes@hotmail.fr>
2019-04-07 12:50:30 +0200

    ajout affichage des erreurs sur le formulaire d'inscription

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -20,52 +20,119 @@ class UserController extends Controller
 
     public function signup()
     {
-        return view('signup');
+        return view('signup', [
+            'inputValues' => [
+                'firstname' =>'',
+                'lastname' => '',
+                'email' => ''
+                ]
+        ]);
     }
 
     public function signupPost()
     {
+
+        // on crée un tableau d'erreurs
+        $error = [];
         //On vérifie que $_POST n'est pas vide
         if (!empty($_POST)){
-        //     // si le formulaire n'est pas vide, on vérifie que chaque champ n'est pas vide
-            $firstName = (!empty($_POST['firstname'])) ? trim($_POST['firstname']): '';
+            // si le formulaire n'est pas vide, on vérifie que chaque champ est rempli
+
+            // si le prénom est rempli, on le stocke dans $firstName, sinon on ajoute une ligne dans le tableau d'erreur 
+            $firstName = (!empty($_POST['firstname'])) ? trim($_POST['firstname']): $error[]= 'Merci d\'indiquer votre prénom';
             //dump($firstName);
-            $lastName = (!empty($_POST['lastname'])) ? trim($_POST['lastname']): '';
+
+            // si le nom est rempli, on le stocke dans $lastName, sinon on ajoute une ligne dans le tableau d'erreur 
+            $lastName = (!empty($_POST['lastname'])) ? trim($_POST['lastname']): $error[]= 'Merci d\'indiquer votre nom';
             //dump($lastName);
+
+            // si l'email est rempli
             if(!empty($_POST['email'])){
                 //on vérifie que l'email est bien un email
+                // si oui
                 if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
                     // on vérifie que l'email n'est pas déjà en BD
                     $checkEmailList = User::all();
                     // dump($checkEmailList);
                     // exit;
                     foreach($checkEmailList as $registeredEmail){
+                        // si l'email est déjà en BD
                         if ($_POST['email'] == $registeredEmail->email){
-                            echo 'L\'adresse existe déjà avec l\'id '. $registeredEmail->id;
-                        } else {
+                            // on ajoute l'erreur dans le tableau d'erreur
+                            $error[]= 'L\'adresse existe déjà avec l\'id '. $registeredEmail->id;
+                        } 
+                        // sinon
+                        else {
+                            // on stocke l'email dans $emailAddress
                             $emailAddress = trim($_POST['email']);
                         }
                     }
+                } 
+                // si l'email n'est pas au bon format
+                else {
+                    // on ajoute l'erreur au tableau d'erreur
+                    $error[]= 'Merci d\'indiquer une adresse e-mail valide';
+                    $emailAddress='';
                 }
+            } // sinon si l'e-mail est vide
+            else {
+                $error[]= 'Merci d\'indiquer une adresse e-mail valide';
+                $emailAddress = '';
             }
             //dump($emailAddress);
+
+            // si password et password vérif sont remplis
             if ((!empty($_POST['password'])) && (!empty($_POST['password_verif']))){
+                //on vérifie que le password est bien 2 fois le même
                 if ($_POST['password'] == ($_POST['password_verif'])){
+                    // si oui
+                    // on le crypte
                     $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-                } else {
-                    echo 'Il y a une erreur sur le password';
+                } 
+                // si non
+                else {
+                    // on stocke l'erreur dans le tableau d'erreurs
+                    $error[]= 'Le mot de passe ne correspond pas';
                 }
+            } // si le champ est vide
+            else {
+                // on l'ajoute en erreur
+                $error[]= 'Merci de renseigner un mot de passe';
+                $password = '';
             }
             //dump($password);
   
         }
-        $newUser = new User;
-        $newUser->firstname = $firstName;
-        $newUser->lastname = $lastName;
-        $newUser->email = $emailAddress;
-        $newUser->password = $password;
+        // On vérifie si il $error est vide
+        if (count($error) == 0) {
+            // on ajoute l'utilisateur en BD
+            $newUser = new User;
+            $newUser->firstname = $firstName;
+            $newUser->lastname = $lastName;
+            $newUser->email = $emailAddress;
+            $newUser->password = $password;
+    
+            $newUser->save();
 
-        $newUser->save();
+            // et on renvoie à la page de connexion
+            return redirect()->route('connexion');
+        }
+        // si il y a des erreurs
+        else {
+            // on les affiche dans le formulaire
+            return view('signup', [
+                'errorList' => $error,
+                'inputValues' => [
+                    'firstname' => $firstName,
+                    'lastname' => $lastName,
+                    'email' => $emailAddress
+                ]
+            ]);
+
+        }
+
+
+
 
 
         return view('signup');
--- resources/views/signup.php
+++ resources/views/signup.php
@@ -9,19 +9,27 @@
                 
                 </p>
             </div>
+            <?php if(!empty($errorList)) : ?>
+                <section id="erreurs" class="alert alert-danger">
+                    <?php foreach($errorList as $currentError): ?>
+                    <p><?= $currentError ?></p>
+                    <?php endforeach;?>                
+                </section>
+            <?php endif; ?>
+
             <form action="<?php route('inscription-post') ?>" method="POST" class="container">
                 
             <div class="row">
 
                     <section class="form-group col-md" >
                         <label for="firstname">Votre prénom</label>
-                        <input type="firstname" class="form-control" id="firstname" name="firstname"aria-describedby="firstnameHelp" placeholder="Prénom" >
+                        <input type="firstname" class="form-control" id="firstname" name="firstname"aria-describedby="firstnameHelp" placeholder="Prénom" value="<?=$inputValues['firstname'] ?>">
                 
                     </section>
 
                     <section class="form-group col-md">
                         <label for="lastname">Votre nom</label>
-                        <input type="lastname" class="form-control" id="lastname" name="lastname" aria-describedby="lastnameHelp" placeholder="Nom" >
+                        <input type="lastname" class="form-control" id="lastname" name="lastname" aria-describedby="lastnameHelp" placeholder="Nom" value="<?= $inputValues['lastname'] ?>">
                         
                     </section>
                 </div>
@@ -30,7 +38,7 @@
 
                     <section class="form-group col-md">
                         <label for="email">Adresse e-mail</label>
-                        <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="exemple@e-mail.com" >
+                        <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="exemple@e-mail.com" value="<?= $inputValues['email']?>" >
                         
                     </section>
     
